0cbeea54436ab5967d33b3d1fd7990c3e4b416b9
johnny <johnny1194@hotmail.com>
2021-06-07 14:01:41 -0400

    adding components

---
+++ project-2-frontend-gif-searcher/src/Components/Header.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+function Header() {
+
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default Header;
---
+++ project-2-frontend-gif-searcher/src/Components/Searcher.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+function Searcher() {
+
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default Searcher;
